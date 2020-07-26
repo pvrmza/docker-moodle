7e80241561c1280a534e19f77faac704938c89bc
pvr.mza <pvr.mza@gmail.com>
2020-07-26 00:46:21 -0300

    added more functionalities v3.7

--- files/foreground.sh
+++ files/foreground.sh
@@ -10,27 +10,46 @@ touch /etc/crontab  /etc/cron.d/php /etc/cron.d/moodlecron
 
 #######
 # timezone
-echo $TZ > /etc/timezone && rm /etc/localtime && \
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
-dpkg-reconfigure -f noninteractive tzdata 
+if test -v TZ; then
+  echo $TZ > /etc/timezone 
+  rm /etc/localtime &&  ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
+  dpkg-reconfigure -f noninteractive tzdata 
+
+  echo "date.timezone=$TZ" > /etc/php/7.2/apache2/conf.d/99_datatime.ini 
+fi
+#######
+# LANG
+if test -v MOODLE_lang; then
+  if ! test -d /var/www/html/lang/$MOODLE_lang; then
+    mkdir -p cd $MOODLE_dataroot/lang && cd $MOODLE_dataroot/lang/
+    wget https://download.moodle.org/download.php/direct/langpack/$MOODLE_VER/$MOODLE_lang.zip -q -O /tmp/temp.zip && unzip /tmp/temp.zip && rm -rf /tmp/temp.zip
+    apt-get update && apt-get -y install language-pack-$MOODLE_lang && dpkg-reconfigure locales 
+  fi
+fi
 
 #######
-# permission check
-echo "placeholder" > /var/moodledata/placeholder
-chown -R www-data:www-data /var/moodledata
-find /var/moodledata -type d -exec chmod 700 {} \; 
-find /var/moodledata -type f -exec chmod 600 {} \; 
+# Building URL
+if test -v VIRTUAL_URL; then
+  # Apache conf
+  echo -e "\nAlias $VIRTUAL_URL /var/www/html \n" >> /etc/apache2/conf-available/apache-moodle.conf
+fi
 
-chown -R root:www-data /var/www/html
-find /var/www/html -type d -exec chmod 750 {} \; 
-find /var/www/html -type f -exec chmod 640 {} \; 
+if test -v MOODLE_sslproxy; then
+  proto=https
+  export MOODLE_reverseproxy=true
+else
+  proto=http
+fi
+
+export MOODLE_wwwroot=$proto://$VIRTUAL_HOST:$VIRTUAL_PORT$VIRTUAL_URL
+
 
 #######
 # check/move admin dir
 if test -v MOODLE_admin; then
-  mv /var/www/html/admin /var/www/html/$MOODLE_admin
+  admin_dir=$(find /var/www/html/ -iname admin_settings_search_form.php | xargs dirname)
+  mv $admin_dir /var/www/html/$MOODLE_admin 2>/dev/null
 fi
-
 
 #######
 # ENVIRONMENT to CONFIG
@@ -42,7 +61,7 @@ global \$CFG;
 " > /var/www/html/config.php
 
 while IFS= read -r line
-do  
+do
   key=`echo $line | cut -d = -f 1 |sed "s/MOODLE_//g"`
   if test $(echo $key | egrep __) ; then
     var=`echo $key | sed "s/__/['/g" | sed "s/$/']/g" `
@@ -50,7 +69,7 @@ do
     var=$key
   fi
 
-  value=`echo $line | cut -d = -f 2- `
+  value=`echo $line | cut -d = -f 2- |sed 's/\"//g'`
   case $value in 
     [0-9]*)        echo "\$CFG->$var=$value;" ;;
     true|false)   echo "\$CFG->$var=$value;" ;;
@@ -58,10 +77,25 @@ do
   esac
 
 done < <(printenv | egrep ^MOODLE_ | sort -u) >> /var/www/html/config.php 
+
 echo "
 require_once(dirname(__FILE__) . '/lib/setup.php'); 
 
 ?>" >> /var/www/html/config.php
+
+#######
+# permission check
+echo "Check permission in $MOODLE_dataroot... "
+echo "placeholder" > $MOODLE_dataroot/placeholder
+chown -R www-data:www-data $MOODLE_dataroot
+find $MOODLE_dataroot -type d -exec chmod 700 {} \; 
+find $MOODLE_dataroot -type f -exec chmod 600 {} \; 
+
+echo "Check permission in www-root... "
+chown -R root:www-data /var/www/html
+find /var/www/html -type d -exec chmod 750 {} \; 
+find /var/www/html -type f -exec chmod 640 {} \; 
+
 
 # start up cron
 /usr/sbin/cron
@@ -69,3 +103,4 @@ require_once(dirname(__FILE__) . '/lib/setup.php');
 # start up apache
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
+
